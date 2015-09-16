6ec8381cc52198846f0d3c7217b6c7747a5f3cd0
LiuXueCheng <895205388@qq.com>
2015-09-16 16:44:27 +0800

    no message

---
+++ ZjhMobile/frameworks/runtime-src/Classes/net/GameSocket.h
@@ -0,0 +1,37 @@
+//  GameSocket.h
+//  ZjhMobile
+//  Created by LiuXueCheng on 15/9/16
+
+#ifndef _GameSocket_h_
+#define _GameSocket_h_
+
+#include "ODSocket.h"
+#include <string>
+#include <thread>
+
+using namespace std;
+
+class GameSocket
+{
+public:
+    static GameSocket* GetSingleton();
+    GameSocket();
+    virtual ~GameSocket();
+    
+    //关闭连接
+    void Close();
+    //连接
+    bool Connect(string ip);
+    //向服务器发消息
+    bool SendMsg(const char* msg, int msgLength, int flags = 0);
+    
+private:
+    //接收服务器消息
+    void ReceiveData();
+    
+private:
+    ODSocket mODSocket;
+    bool mbConnected;
+};
+
+#endif
